5234b12dec2e762e8d1d51a2d53dd4d7ea05501d
dinadehbi <147596316+dinadehbi@users.noreply.github.com>
2024-01-16 07:42:25 +0100

    Update script.js

--- chat brief/script.js
+++ chat brief/script.js
@@ -1,18 +1,20 @@
 let cont = document.getElementById("cont");
 let chatMessages = document.getElementById("chat-messages");
 let input = document.getElementById("inp");
-let demo = document.querySelector("h3");
+let h3 = document.querySelector("h3");
 var currentPerson = "Saalali";
-demo.innerHTML = currentPerson + " is chatting ...";
+h3.innerHTML = currentPerson + " is chatting ...";
+
 let btn1 = document.querySelector(".btn1")
 let btn2 = document.querySelector(".btn2")
+
 btn1.addEventListener("click",function(){
     currentPerson = "Saalali"
-    demo.innerHTML = currentPerson + " is chatting ...";
+    h3.innerHTML = currentPerson + " is chatting ...";
 })
 btn2.addEventListener("click",function(){
     currentPerson = "Dina";
-    demo.innerHTML = currentPerson + " is chatting ...";
+    h3.innerHTML = currentPerson + " is chatting ...";
 })
 
 function send() {
@@ -22,18 +24,26 @@ function send() {
     let message = `${currentPerson}: ${input.value} <span id="spn">${hour}</span>`;
     input.value = "";
     var messageDisplay = document.createElement("p")
-    messageDisplay.style.backgroundColor = "blue"
+    messageDisplay.style.backgroundColor = "rgb(87, 87, 246)"
     messageDisplay.innerHTML = message;
     chatMessages.appendChild(messageDisplay);
+  
+    
     }   
     if(currentPerson == "Dina"){
     let date = new Date();
     let hour = date.getHours() + ":" + date.getMinutes();
-    var message = `${currentPerson}: ${input.value} <span id="spn">${hour}</span>`;
+    let val = input.value;
+    var message = `${currentPerson}: ${val} <span id="spn">${hour}</span>`;
     input.value = "";
- var messageDisplay = document.createElement("p")
+ var messageDisplay = document.createElement("p");
  messageDisplay.style.backgroundColor = "rgb(255, 8, 160)"
     messageDisplay.innerHTML = message;
     chatMessages.appendChild(messageDisplay);
 }
+}
+
+function remo(){
+    chatMessages.innerHTML = "";
+
 }
